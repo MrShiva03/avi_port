bb7f665246c20831cc4cfbd4d5f629de37b3e3b4
Shiva Mishra <shivamisha09@gmail.com>
2025-04-30 16:11:29 +0530

    new

--- assets/js/scr.js
+++ assets/js/scr.js
@@ -12,7 +12,7 @@ menuIcon.onclick = () => {
 
 
 let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('hearder nav a');
+let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
